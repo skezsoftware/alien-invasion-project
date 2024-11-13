8e581f7cc2ea75ab89b930ffbaf9441f7089195a
Daniel Vasquez <dwvasquez97@gmail.com>
2024-11-12 19:37:56 -0800

    added many changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,13 +31,17 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        # Start Alien Invasion in an inactive state.
+        self.game_active = True 
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
 
@@ -131,6 +135,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
     def _check_fleet_edges(self):
         """Respond appropriately if any aliens have reached an edge."""
         for alien in self.aliens.sprites():
@@ -157,7 +164,15 @@ class AlienInvasion:
             # Pause.
             sleep(0.5)
         else:
-            self.stats.game_active = False  
+            self.game_active = False      
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                self._ship_hit()
+                break   
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
